e88f033e090ce564087f6c0734c5960a0c1fcbea
Keshav <keshavkumaraster@gmail.com>
2026-06-01 14:14:27 +0530

    remove hardcoded api keys

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -3,14 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC9oiTcmtCAYs6B0x9bYRU1cZa1jwN6pw4",
-  authDomain: "ai-travel-planner-787c4.firebaseapp.com",
-  projectId: "ai-travel-planner-787c4",
-  storageBucket: "ai-travel-planner-787c4.firebasestorage.app",
-  messagingSenderId: "372841557916",
-  appId: "1:372841557916:web:a0eacc98a5a3bcdea6a259",
-  measurementId: "G-5GSGR3C8CR"
-};
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+}
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
